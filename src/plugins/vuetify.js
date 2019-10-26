338c755e95206e6afeaf45faf2e1bc47e0ce48d2
Jason Priem <jp@jasonpriem.org>
2019-10-26 10:17:08 -0700

    add vuetify.

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors'
+
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: colors.green,
+                secondary: colors.grey.darken1,
+                accent: colors.shades.black,
+            }
+        },
+    },
+
+    icons: {
+        iconfont: 'mdi',
+    },
+});
