198d438e041ea3eec4b9c9433bba2e00fbc0c256
Jason Priem <jp@jasonpriem.org>
2019-10-29 17:16:16 -0700

    bars move with real data.

--- src/views/ScenarioTabs/SliderTab.vue
+++ src/views/ScenarioTabs/SliderTab.vue
@@ -14,7 +14,7 @@
                             <v-col cols="2">
                                 <div class="bar-wrapper">
                                     <div class="bar-fill"></div>
-                                    <div class="bar" :style="{height: costPercent+'%'}"></div>
+                                    <div class="bar cost" :style="{height: costPercent+'%'}"></div>
                                 </div>
                             </v-col>
                         </v-row>
@@ -22,10 +22,28 @@
                             ${{this.cost}}
                         </v-row>
                     </v-col>
+                    <v-col>
+                        <v-row>
+                            <v-col cols="2">
+                                <div class="bar-wrapper">
+                                    <div class="bar delayed bar-fill"></div>
+                                    <div class="bar paid instant" :style="{height: usage.subr+'%'}"></div>
+                                    <div class="bar free instant" :style="{height: usage.oa+'%'}"></div>
+                                    <div class="bar free instant" :style="{height: usage.backfile+'%'}"></div>
+                                    <div class="bar free instant" :style="{height: usage.asn+'%'}"></div>
+                                </div>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            {{instantUsage}} instant access
+                            <pre>{{usage}}</pre>
+                        </v-row>
+                    </v-col>
                 </v-row>
             </v-col>
             <v-col cols="4">
                 {{ subscribedJournals.length }} subscribed journals
+                total usage {{totalUsage}}
             </v-col>
         </v-row>
 
@@ -38,7 +56,8 @@
         name: "SliderTab",
         data() {
             return {
-                costPercent: 50
+                costPercent: 50,
+                totalUsage: 0,
             }
         },
         computed: {
@@ -46,51 +65,107 @@
                 return .01 * this.costPercent * this.data._summary.cost_bigdeal_projected
             },
 
-            subscribedJournals(){
+            subscribedJournals() {
+                return this.data.journals.filter(j=>!!j.subscribed)
+            },
+            usage() {
+                const ret = {
+                    oa: 0,
+                    backfile: 0,
+                    asn: 0,
+                    ill: 0,
+                    otherDelayed: 0,
+                    subr: 0,
+                }
+                this.data.journals.forEach(j=>{
+                    ret.oa += j.use_groups_free_instant.oa
+                    ret.backfile += j.use_groups_free_instant.backfile
+                    ret.asn += j.use_groups_free_instant.social_networks
+
+                    if (j.subscribed){
+                        ret.subr += j.use_groups_if_subscribed.subscription
+                    }
+                    else {
+                        ret.ill += j.use_groups_if_not_subscribed.ill
+                        ret.otherDelayed += j.use_groups_if_not_subscribed.other_delayed
+                    }
+                })
+                const total = Object.values(ret).reduce((a,b)=>a+b) || 1
+                Object.keys(ret).forEach(k=>{
+                    ret[k] = 100 *  ret[k] / total
+                })
+
+
+                return ret
+            },
+            instantUsage(){
+                const usage = this.usage
+                return usage.oa + usage.backfile + usage.asn + usage.subr
+            },
+            instantUsagePercent() {
+            },
+            numSubr() {
+                return 0
+            },
+            numJournals(){
+                return this.data && this.data.journals.length
+            }
+
+
+        },
+        methods: {
+            updateJournals(){
+                if (!this.data) return
+
                 const myMax = this.cost
-                let mySpendSoFar = this.data.journals.map(j=>j.cost_ill).reduce((a,b)=>a+b)
-                const ret = new Set([])
+
+                // unsubscribe all
+                this.data.journals.forEach(j=>j.subscribed=false)
+
+                // ILL cost must be paid regardless
+                let mySpendSoFar = this.data.journals.map(j => j.cost_ill).reduce((a, b) => a + b)
 
                 // subscribe to journals where subr is cheaper than ILL
-                this.data.journals.forEach(j=>{
-                    if (j.cost_subscription_minus_ill < 0){
-                        ret.add(j)
+                this.data.journals.forEach(j => {
+                    if (j.cost_subscription_minus_ill < 0) {
+                        j.subscribed = true
                         mySpendSoFar += j.cost_subscription_minus_ill
                     }
                 })
 
-
                 // subscribe to as many other journals as we can afford
-                this.data.journals.forEach(j=>{
+                this.data.journals.forEach(j => {
                     mySpendSoFar += j.cost_subscription_minus_ill
-                    if (mySpendSoFar <= myMax){
-                        ret.add(j)
+                    if (mySpendSoFar <= myMax) {
+                        j.subscribed = true
                     }
                 })
-                return Array.from(ret)
-            },
-            instantUsage() {
-                return 142
-            },
-            instantUsagePercent() {
-                return 55
-            },
-            numSubr() {
-                return 0
             }
+        },
+        mounted(){
 
         },
-        methods: {
-        },
+        watch: {
+            costPercent: function(to, from){
+                this.updateJournals()
+            },
+            numJournals: function(to, from){
+                this.updateJournals()
+            }
+
+        }
     }
 </script>
 
-<style lang="scss" >
+<style lang="scss">
     .v-slider--vertical {
         min-height: 500px !important;
+        margin: 0 !important;
     }
+
     .bar-wrapper {
         height: 100%;
+        min-height: 500px;
         display: flex;
         flex-direction: column;
 
@@ -100,7 +175,15 @@
         }
 
         .bar {
-            background: rebeccapurple;
+            &.cost {
+                background: #555;
+            }
+            &.free {
+                background: green;
+            }
+            &.paid.instant {
+                background: dodgerblue;
+            }
         }
     }
 
