6918665d45aa2520b08676853a1ae28835dccab3
Jason Priem <jp@jasonpriem.org>
2019-11-04 15:53:12 -0500

    login page works.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,37 @@
 <template>
-  <v-container class="login">
-    <h1 class="display-3">Log in</h1>
+    <v-container class="login">
+        <v-row class="pt-12"  align="center" justify="center">
+                <v-card width="400">
+                    <v-card-title>
+                        Log in to your account
+                    </v-card-title>
+                    <v-card-text>
+                        <div>
+                        <v-text-field
+                                label="Username"
+                                placeholder="Username"
+                                outlined
+                        ></v-text-field>
+                        <v-text-field
+                                label="Password"
+                                placeholder="Password"
+                                outlined
+                                type="password"
+                        ></v-text-field>
+
+                        </div>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn large depressed color="primary">
+                            Log in
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+        </v-row>
 
 
-  </v-container>
+    </v-container>
 </template>
 
 <script>
--- src/views/PurchaseResult.vue
+++ src/views/PurchaseResult.vue
@@ -1,46 +1,51 @@
 <template>
     <v-container class="purchase">
-        <v-card v-if="$route.params.result==='success'">
-            <v-card-actions>
-                <v-alert prominent type="success">
-                    <h1 class="display-1">Purchase successful!</h1>
-                    <div>
+        <v-row class="pt-12" align="center" justify="center">
 
-                        Thanks your support! You'll receive an email from us shortly with instructions on how to upload
-                        your COUNTER files and set up your new account!
-                    </div>
+            <v-card width="600" v-if="$route.params.result==='success'">
+                <v-card-actions>
+                    <v-alert prominent type="success">
+                        <h1 class="display-1">Purchase successful!</h1>
+                        <div>
 
-                </v-alert>
-            </v-card-actions>
+                            Thanks your support! You'll receive an email from us shortly with instructions on how to upload
+                            your COUNTER files and set up your new account!
+                        </div>
 
-            <v-card-actions>
-                <v-btn depressed color="primary" to="/">
-                    ok
-                </v-btn>
-            </v-card-actions>
-        </v-card>
+                    </v-alert>
+                </v-card-actions>
 
-        <v-card v-if="!$route.params.result !== 'success'">
-            <v-card-actions v-if="$route.params.result==='cancelled'">
-                <v-alert prominent type="error">
-                    <h1 class="display-1">Something went wrong with your purchase!</h1>
-                    <div>
+                <v-card-actions>
+                    <v-btn depressed color="primary" to="/">
+                        ok
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
 
-                        We're sorry, but there was some kind of problem and your order didn't work. You may want to try again...or feel free to contact us to let us know about the bug. We'll make sure to get it figured out.
-                    </div>
 
-                </v-alert>
-            </v-card-actions>
-            <v-card-actions>
-                <v-btn depressed to="/purchase">
-                    Try again
-                </v-btn>
-                <v-btn depressed href="mailto:team@ourresearch.org">
-                    report bug
-                </v-btn>
-            </v-card-actions>
 
-        </v-card>
+            <v-card width="600" v-if="$route.params.result !== 'success'">
+                <v-card-actions>
+                    <v-alert prominent type="error">
+                        <h1 class="display-1">Something went wrong with your purchase!</h1>
+                        <div>
+
+                            We're sorry, but there was some kind of problem and your order didn't work. You may want to try again...or feel free to contact us to let us know about the bug. We'll make sure to get it figured out.
+                        </div>
+
+                    </v-alert>
+                </v-card-actions>
+                <v-card-actions>
+                    <v-btn depressed to="/purchase">
+                        Try again
+                    </v-btn>
+                    <v-btn depressed href="mailto:team@ourresearch.org">
+                        report bug
+                    </v-btn>
+                </v-card-actions>
+
+            </v-card>
+        </v-row>
 
 
     </v-container>
