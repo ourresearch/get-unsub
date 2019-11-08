d52e88e9a7de6a008012b3c9f12453f08ebb574e
Jason Priem <jp@jasonpriem.org>
2019-11-08 15:43:19 -0500

    redirect everything to https.

--- server.js
+++ server.js
@@ -9,10 +9,23 @@ let app = express();
 // https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
 app.use(serveStatic(__dirname + "/dist"));
 
-app.get('*', function(req, res) {
-  res.sendfile('./dist/index.html');
-});
+// app.get('*', function(req, res) {
+//   res.sendfile('./dist/index.html');
+// });
 
+
+
+// redirect to https. untested yet.
+app.get('*', function (req, res) {
+
+    if (req.protocol !== "https") {
+        const redirectUrl =  "https://journals.unpaywall.org" + req.path
+        res.redirect(redirectUrl)
+    }
+    else {
+        res.sendfile('./dist/index.html');
+    }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
