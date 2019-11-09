8c6895af6e3d10996e216e01e063c9eb72aaab93
Jason Priem <jp@jasonpriem.org>
2019-11-09 16:48:24 -0500

    move app bar into separate component.

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,6 @@
 <template>
     <v-app>
-
-
-        <v-app-bar app clipped-left>
-            <v-toolbar-title class="headline">
-                <router-link to="/">
-                    <img class="mt-3" style="margin: 12px 28px 0" src="./assets/logo.png" alt="">
-                </router-link>
-            </v-toolbar-title>
-
-
-            <v-progress-linear
-                    :active="loading"
-                    :indeterminate="loading"
-                    absolute
-                    bottom
-                    color="green"
-            ></v-progress-linear>
-
-
-            <v-divider
-                    class="ml-8"
-                    vertical
-                    v-if="$store.getters.isLoggedIn"
-            ></v-divider>
-
-            <v-toolbar-items class="breadcrumbs" v-if="$store.getters.isLoggedIn">
-                <v-btn class="pa-0 pl-4" text :to="`/a/${account.id}`" active-class="">
-                    <v-icon left>mdi-account</v-icon>
-                    <v-icon v-if="selectedPkg">mdi-chevron-right</v-icon>
-                    <span v-if="!selectedPkg">{{account.name}}</span>
-                </v-btn>
-                <v-btn text class="pl-4" v-if="selectedPkg" :to="`/a/${account.id}/${selectedPkg.id}`">
-                    <v-icon left>mdi-package-variant</v-icon>
-                    <v-icon v-if="selectedScenario">mdi-chevron-right</v-icon>
-                    <span v-if="!selectedScenario">{{selectedPkg.name}}</span>
-                </v-btn>
-                <v-btn class="pl-4" v-if="selectedScenario" text>
-                    <v-icon left>mdi-chart-bar</v-icon>
-                    {{selectedScenario.name}}
-                </v-btn>
-
-
-            </v-toolbar-items>
-
-
-            <v-spacer></v-spacer>
-
-            <v-toolbar-items>
-                <v-btn text color="primary" to="/purchase">Pricing</v-btn>
-                <v-btn text to="/support">Help</v-btn>
-                <v-btn text to="/login" v-if="!$store.getters.isLoggedIn">Log in</v-btn>
-
-                <v-btn text
-                       v-if="summary"
-                       class="px-2 toolbar-summary"
-                       @click="$store.commit('toggleConfigsOpen')">
-                    <v-icon>mdi-settings</v-icon>
-                </v-btn>
-            </v-toolbar-items>
-
-            <div class="not-logged-in pl-2" v-if="!$store.getters.isLoggedIn">
-            </div>
-
-            <div class="logged-in title" v-if="$store.getters.isLoggedIn && summary">
-            </div>
-
-            <!--            <div class="logged-in" v-if="$store.getters.isLoggedIn">-->
-            <!--                <v-btn text icon @click="drawerRight = !drawerRight">-->
-            <!--                    <v-icon>mdi-settings</v-icon>-->
-            <!--                </v-btn>-->
-            <!--            </div>-->
-
-
-        </v-app-bar>
+        <app-bar></app-bar>
 
 
         <v-navigation-drawer v-if="summary" dark v-model="$store.state.configsOpen" app right class="pt-12">
@@ -139,6 +66,7 @@
     import SingleJournal from "./components/SingleJournal"
     import SliderTab from "./views/ScenarioTabs/SliderTab"
     import NavSidebar from "./components/NavSidebar"
+    import AppBar from "./components/AppBar"
 
     export default {
         name: 'App',
@@ -149,6 +77,7 @@
             SliderTab,
             StartupTutorial,
             NavSidebar,
+            AppBar,
         },
         data: () => ({
             drawerRight: false,
---
+++ src/components/AppBar.vue
@@ -0,0 +1,119 @@
+<template>
+
+
+    <v-app-bar app clipped-left>
+        <v-toolbar-title class="headline">
+            <router-link to="/">
+                <img class="mt-3" style="margin: 12px 28px 0" src="../assets/logo.png" alt="">
+            </router-link>
+        </v-toolbar-title>
+
+
+        <v-progress-linear
+                :active="loading"
+                :indeterminate="loading"
+                absolute
+                bottom
+                color="green"
+        ></v-progress-linear>
+
+
+        <v-divider
+                class="ml-8"
+                vertical
+                v-if="$store.getters.isLoggedIn"
+        ></v-divider>
+
+        <v-toolbar-items class="breadcrumbs" v-if="$store.getters.isLoggedIn">
+            <v-btn class="pa-0 pl-4" text :to="`/a/${account.id}`" active-class="">
+                <v-icon left>mdi-account</v-icon>
+                <v-icon v-if="selectedPkg">mdi-chevron-right</v-icon>
+                <span v-if="!selectedPkg">{{account.name}}</span>
+            </v-btn>
+            <v-btn text class="pl-4" v-if="selectedPkg" :to="`/a/${account.id}/${selectedPkg.id}`">
+                <v-icon left>mdi-package-variant</v-icon>
+                <v-icon v-if="selectedScenario">mdi-chevron-right</v-icon>
+                <span v-if="!selectedScenario">{{selectedPkg.name}}</span>
+            </v-btn>
+            <v-btn class="pl-4" v-if="selectedScenario" text>
+                <v-icon left>mdi-chart-bar</v-icon>
+                {{selectedScenario.name}}
+            </v-btn>
+
+
+        </v-toolbar-items>
+
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn text color="primary" to="/purchase">Pricing</v-btn>
+            <v-btn text to="/support">Help</v-btn>
+            <v-btn text to="/login" v-if="!$store.getters.isLoggedIn">Log in</v-btn>
+
+            <v-btn text
+                   v-if="summary"
+                   class="px-2 toolbar-summary"
+                   @click="$store.commit('toggleConfigsOpen')">
+                <v-icon>mdi-settings</v-icon>
+            </v-btn>
+        </v-toolbar-items>
+
+        <div class="not-logged-in pl-2" v-if="!$store.getters.isLoggedIn">
+        </div>
+
+        <div class="logged-in title" v-if="$store.getters.isLoggedIn && summary">
+        </div>
+
+
+    </v-app-bar>
+
+
+</template>
+
+<script>
+
+    export default {
+        name: "AppBar",
+        components: {},
+        data() {
+            return {}
+        },
+        methods: {
+            increment() {
+            }
+        },
+        computed: {
+            summary() {
+                return this.$store.getters.summary
+            },
+            loading() {
+                return this.$store.state.tabDataLoading
+                    || this.$store.getters.wizardLoading
+            },
+            count() {
+            },
+            account() {
+                return this.$store.state.user
+            },
+            selectedPkg() {
+                return this.$store.getters.selectedPkg
+            },
+            selectedScenario() {
+                return this.$store.getters.selectedScenario
+            },
+        },
+        created() {
+        },
+        mounted() {
+            this.$store.commit("selectPkg", this.$route.params.pkgId)
+            this.$store.commit("clearScenario")
+
+            console.log("pkg: mount up", this.$route.params)
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
