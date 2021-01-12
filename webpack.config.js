9be723f3148ac33331dc8a0455bc2c884227ce42
Jason Priem <jp@jasonpriem.org>
2021-01-12 15:48:56 -0800

    add webpack config to fix SASS compile error after upgrade.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// added to chase down a sass compile error, as recommended in the
+// docs here: https://vuetifyjs.com/en/getting-started/frequently-asked-questions/#questions
+module.exports = {
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require('sass'),
+            indentedSyntax: true // optional
+          },
+          // Requires sass-loader@^8.0.0
+          options: {
+            implementation: require('sass'),
+            sassOptions: {
+              indentedSyntax: true // optional
+            },
+          },
+        },
+      ],
+    },
+  ],
+}
