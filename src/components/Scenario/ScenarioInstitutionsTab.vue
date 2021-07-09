321a438c041ae376f9ffb23e6aa1f051a42c088f
Jason Priem <jp@jasonpriem.org>
2021-07-09 14:12:13 -0700

    save point.

--- src/components/Scenario/ScenarioInstitutionsTab.vue
+++ src/components/Scenario/ScenarioInstitutionsTab.vue
@@ -28,7 +28,7 @@
     </div>
 
     <div class="d-flex mb-6">
-      <v-btn dark color="primary" class="mr-2">send</v-btn>
+      <v-btn dark color="primary" class="mr-2" @click="openSendDialog">send</v-btn>
       <v-btn dark color="primary" class="mr-2" @click="openIncludeExcludeDialog">include/exclude</v-btn>
       <v-spacer/>
       <v-text-field
@@ -57,35 +57,101 @@
     </div>
 
 
+    <v-dialog v-model="dialogs.includeExclude" persistent max-width="400">
+      <v-card>
+        <v-card-title>
+          <v-icon left>mdi-bank-outline</v-icon>
+          Modify members
+        </v-card-title>
+        <div class="pa-4">
+          <div>
+            You've selected {{ selectedInstitutions.length }} institutions. Do you want to <strong>include</strong> or
+            <strong>exclude</strong> them from this scenario?
+          </div>
+          <v-radio-group v-model="includeOrExcludeMembers">
+            <v-radio
+                label="Include"
+                value="include"
+            />
+            <v-radio
+                label="Exclude"
+                value="exclude"
+            />
+          </v-radio-group>
+          <div v-if="includeOrExcludeMembers === 'include'">
+            Clicking "Include" below will ensure that your {{ selectedInstitutions.length }} selected institutions are
+            included in the modelling for this scenario; their usage, citations, and authorships will be considered.
+          </div>
+          <div v-if="includeOrExcludeMembers === 'exclude'">
+            Clicking "Exclude" below will ensure that your {{ selectedInstitutions.length }} selected institutions are
+            <strong>not</strong> included in the modelling for this scenario; their usage, citations, and authorships
+            will be ignored.
+          </div>
+
+        </div>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn depressed
+                 @click="dialogs.includeExclude = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn depressed
+                 @click="saveIncludeExclude"
+                 color="primary"
+                 :loading="isSaving"
+          >
+            {{ includeOrExcludeMembers }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
 
+    <v-dialog v-model="dialogs.send" persistent max-width="600">
+      <v-card>
+        <v-card-title>
+          <v-icon left>mdi-bank-outline</v-icon>
+          Send scenario to members
+        </v-card-title>
+        <div class="pa-4">
+          <div>
+            You've selected {{ selectedInstitutions.length }} member institutions, and you're about to send a copy of
+            this scenario to them. Clicking "Send" below will do two things:
+          </div>
+          <ol>
+            <li>
+              Send a copy of this scenario to the selected institution. They will be able to see your proposed
+              title-by-title subscriptions and parameter settings. However, they won't be able to modify them, since
+              they are just getting a copy. In their own dashboard, they can add some proposed title-by-title
+              subscriptions, and then send these back to you. You can download their proposed additional subscriptions
+              in the "export" tab of this scenario.
+            </li>
+            <li>
+              Send an automated email to the selected institutions, letting them know you've shared this scenario with
+              them, and requesting their feedback. If you've already shared the scenario before, it'll stay
+              shared...they'll just get another reminder email.
+            </li>
+          </ol>
 
-
-<v-dialog v-model="dialogs.includeExclude" max-width="400">
-            <v-card>
-              <v-card-title>
-                  <v-icon left>mdi-bank-outline</v-icon>
-                  Include/exclude members
-              </v-card-title>
-                <v-card-actions>
-                    <v-spacer/>
-                    <v-btn depressed
-                           @click="dialogs.includeExclude = false"
-                    >
-                        Cancel
-                    </v-btn>
-                    <v-btn depressed
-                           @click="createPublisher"
-                           color="primary"
-                           :loading="newPublisherLoading"
-                           :disabled="institutionIsDemo"
-
-                    >
-                        OK
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+        </div>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn depressed
+                 @click="dialogs.send = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn depressed
+                 @click="saveSend"
+                 color="primary"
+                 :loading="isSaving"
+          >
+            send
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
   </v-card>
 </template>
@@ -95,6 +161,10 @@ import {mapActions, mapGetters, mapMutations} from 'vuex'
 import {api, urlBase} from "../../api";
 import {saveScenarioInstitutions} from "../../shared/scenario";
 
+
+const makeInstitution = function () {
+
+}
 
 export default {
   name: "ScenarioInstitutionsTab",
@@ -115,6 +185,7 @@ export default {
       multiSelect: false,
       someSelected: false,
       selectedInstitutions: [],
+      includeOrExcludeMembers: "include",
       dialogs: {
         send: false,
         includeExclude: false,
@@ -147,41 +218,59 @@ export default {
         this.includedIds = myInstitutionPackageIds
       }
     },
-    openIncludeExcludeDialog(){
+    openIncludeExcludeDialog() {
       this.dialogs.includeExclude = true
+      this.includeOrExcludeMembers = "include"
     },
-    async openDialog() {
-      this.isLoading = true
-      this.startGlobalLoading()
-      console.log("openDialog getting this url", this.apiUrl)
-      try {
-        const resp = await api.get(this.apiUrl)
-        this.institutions = resp.data.institutions
-        console.log("api get member institutions resp", resp)
-      } finally {
-        this.isLoading = false
-        this.finishGlobalLoading()
-      }
-      this.selectedInstitutions = this.institutions.filter(i => i.included)
+    openSendDialog() {
+      this.dialogs.send = true
     },
-    async saveDialog() {
+    async saveSend() {
+      console.log("save send")
       this.isSaving = true
-      this.startGlobalLoading()
 
       console.log("POSTing institution IDs to server", this.selectedInstitutions)
-      const resp = await saveScenarioInstitutions(this.scenarioId, this.selectedInstitutions)
-
+      const selectedPackageIds = this.selectedInstitutions.map(i => i.package_id)
+      const resp = await saveScenarioInstitutions(this.scenarioId, selectedPackageIds)
       console.log("institution IDs POSTed successfully. refreshing scenario.", resp)
 
       // refresh our current scenario, and also refresh this row on the publisher page
       await this.refreshSelectedScenario()
       await this.$store.dispatch("refreshPublisherScenario", this.scenarioId)
-
       console.log("scenario updated w new institutions.", resp)
 
       this.isSaving = false
-      this.finishGlobalLoading()
+      this.snackbar("Scenario shared and notifications sent.")
+      this.dialogs.send = false
+    },
+    async saveIncludeExclude() {
+      this.isSaving = true
+
+      console.log("POSTing institution IDs to server", this.selectedInstitutions)
+      const selectedPackageIds = this.selectedInstitutions.map(i => i.package_id)
+      const resp = await saveScenarioInstitutions(this.scenarioId, selectedPackageIds)
+      console.log("institution IDs POSTed successfully. refreshing scenario.", resp)
+
+      // refresh our current scenario, and also refresh this row on the publisher page
+      await this.refreshSelectedScenario()
+      await this.$store.dispatch("refreshPublisherScenario", this.scenarioId)
+      console.log("scenario updated w new institutions.", resp)
+
+      this.isSaving = false
+      this.dialogs.includeExclude = false
       this.snackbar("Institutions updated.")
+    },
+    async getInstitutions() {
+      this.isLoading = true
+      this.startGlobalLoading()
+      console.log("getInstitutions getting this url", this.apiUrl)
+      try {
+        const resp = await api.get(this.apiUrl)
+        this.institutions = resp.data.institutions
+      } finally {
+        this.isLoading = false
+        this.finishGlobalLoading()
+      }
     },
   },
   computed: {
@@ -210,7 +299,7 @@ export default {
   created() {
   },
   mounted() {
-    this.openDialog()
+    this.getInstitutions()
   },
   watch: {
     // includedIds: {
